8bf43e61c31eaf8bcdc18bb19ccd7bd6c0974115
iamkaleb <kalebdmoran@gmail.com>
2020-08-26 14:51:27 -0500

    can finally see customer info

--- src/components/account/Profile.js
+++ src/components/account/Profile.js
@@ -2,9 +2,9 @@ import React, {useState, useEffect} from 'react'
 
 const Profile = () => {
     
-    const [user, setUser] = useState([])
+    const [currentUser, setCurrentUser] = useState({})
 
-    const getUser = () => {
+    const getCurrentUser = () => {
         return fetch('http://localhost:8000/customers', {
             "method": "GET",
             "headers": {
@@ -13,23 +13,31 @@ const Profile = () => {
             }
         }).then(response => response.json())
         .then(user => {
-            console.log(user)
-            setUser(user)
+            console.log(user[0])
+            const loggedInUser = {
+                "id": user[0].id,
+                "first_name": user[0].user.first_name,
+                "last_name": user[0].user.last_name,
+                "email": user[0].user.email,
+                "address": user[0].address,
+                "phone_number": user[0].phone_number
+            }
+            setCurrentUser(loggedInUser)
         })
     }
 
     useEffect(() => {
-        getUser();
+        getCurrentUser();
     }, [])
 
     return (
         <section>
             <h1>Profile</h1>
 
-            <p>First name: {user.first_name}</p>
-            <p>Last name: {user.last_name}</p>
-            <p>Address: {user.address}</p>
-            <p>Phone number: {user.phone_number}</p>
+            <p>First name: {currentUser.first_name}</p>
+            <p>Last name: {currentUser.last_name}</p>
+            <p>Address: {currentUser.address}</p>
+            <p>Phone number: {currentUser.phone_number}</p>
             <p>Payment options:</p>
             <p>Order history:</p>
             <button>Add payment option</button>
