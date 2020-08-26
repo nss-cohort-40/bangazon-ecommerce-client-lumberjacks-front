06e83269908e8d367768ce9475e727a0c948442e
Eli Lavoie <storey14@googlemail.comgit config --global user.name Eli>
2020-08-26 09:51:35 -0500

    added link to product details to product card creation

--- src/components/product/ProductCard.js
+++ src/components/product/ProductCard.js
@@ -1,12 +1,17 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
 const ProductCard = props => {
-
+    const link = "/" + props.product.id
     return (
         <div className='product-card'>
             <div className='product-card-content'>
                 <img src={props.product.image} width={240} alt='thumbnail' />
-                <h4>{props.product.title}</h4>
+                <h4>
+                    <Link to={link}>
+                        {props.product.title}
+                    </Link>
+                </h4>
             </div>
         </div>
     )
