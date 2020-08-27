87b38eb9353e9aef22d5666d1bd2c4c82f1f420d
Eli Lavoie <storey14@googlemail.comgit config --global user.name Eli>
2020-08-27 11:03:18 -0500

    styling on product details page

--- src/components/product/ProductDetail.js
+++ src/components/product/ProductDetail.js
@@ -1,9 +1,11 @@
 import React, {useState, useEffect} from 'react'
 import dataManager from '../../modules/dataManager'
+import './ProductDetail.css'
 
 const ProductDetail = props => {
     
     const [product, setProduct] = useState({})
+
     const getProduct = () => {
         console.log(props.productId)
         dataManager.get("products", props.productId)
@@ -23,9 +25,17 @@ const ProductDetail = props => {
 
     return(
         <>
-            <img src={product.image}/>
-            <h1>{product.title}</h1>
-            <p>{product.description}</p>
+        <div className="details_page">
+            <div className="picture">
+                <img className="product-picture"  src={product.image}/>
+            </div>
+            <div className="information">
+                <h1>{product.title}</h1>
+                <h3>{product.quantity} in stock</h3>
+                <p>{product.description}</p>
+                <button>Add to Cart</button> | ${product.price}
+            </div>
+        </div>
         </>
     )
 }
