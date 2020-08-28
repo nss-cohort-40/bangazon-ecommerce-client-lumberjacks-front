fd4d4261f4659f0c68f4eb9f3c77adbba63efc64
David Everett <davideverett1989@gmail.com>
2020-08-28 09:05:44 -0500

    Changed profile navbar span to a link.

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -25,7 +25,7 @@ const NavBar = () => {
                     <Link className="nav-link" to="/products/cart"> Shopping Cart </Link>
                 </li>
                 <li>
-                    <span className="nav-link" to="/profile"> Profile </span>
+                    <Link className="nav-link" to="/profile"> Profile </Link>
                 </li>
                 <li>
                     <Link className="nav-link" to="/login">Login</Link>
