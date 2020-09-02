e3bc5d49c2d79ae20208fe27a7bd8253d1ea4dfc
Brian Cravens <brian.cravens303@gmail.com>
2020-09-02 14:13:13 -0500

    ..

--- src/components/order/OrderDetails.js
+++ src/components/order/OrderDetails.js
@@ -9,6 +9,7 @@ const OrderDetails = (props) => {
     const [isLoading, setIsLoading] = useState(false)
     const [toggle, setToggle] = useState(false)
     const [hasOrder, setHasOrder] = useState(false)
+    const [hasCart, setHasCart] = useState(false)
 
     const getOrder = () => {
         setIsLoading(true)
@@ -33,11 +34,16 @@ const OrderDetails = (props) => {
         shoppingCart.shoppingCart('products/cart')
         .then((cart) => {
             if (cart == undefined){
-            }else{
-            setCart(cart)
-            setIsLoading(false)
             }
-        })   
+            if (cart.length == 0){
+                setHasCart(false)
+            }
+            else{
+            setCart(cart)
+            setHasCart(true)
+            setIsLoading(false)
+            } 
+    })   
         .catch((err) => console.error('There as an issue with getting all products:', err));  
     },[toggle]);
 
@@ -50,10 +56,10 @@ const OrderDetails = (props) => {
         <div className="OrderDetail">
             <h1>Shopping Cart</h1>
             <div className="product-container">
-            {hasOrder?cart.map((singleProduct, i) => <OrderProductCard setToggle={setToggle} toggle={toggle} key={i+1} product={singleProduct}/>):<p>There's Nothing Here!!</p>}
+            {hasOrder && hasCart? cart.map((singleProduct, i) => <OrderProductCard setToggle={setToggle} toggle={toggle} key={i+1} product={singleProduct}/>):<p>There's Nothing Here!!</p>}
             </div>
             <div className="delete-order-container">
-            {hasOrder?<button className="delete-order" onClick = {()=>handleDelete(order[0].id)} disabled={isLoading}>Cancel Order</button>:<p></p>}
+            {hasOrder && hasCart? <button className="delete-order" onClick = {()=>handleDelete(order[0].id)} disabled={isLoading}>Cancel Order</button>:<p></p>}
             </div>
         </div>
     )
