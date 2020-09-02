fc9b0acb624b304e4cec004597de3db5f6639c63
Brian Cravens <brian.cravens303@gmail.com>
2020-09-02 10:53:37 -0500

    Cancel Order Complete

--- src/components/order/OrderDetails.js
+++ src/components/order/OrderDetails.js
@@ -1,24 +1,42 @@
 import React, { useState, useEffect } from 'react';
 import OrderProductCard from '../order/OrderProductCard'
 import shoppingCart from '../../hooks /shoppingCart'
+import dataManager from '../../modules/dataManager'
 
 const OrderDetails = (props) => {
     const [cart, setCart] = useState([]);
+    const [order, setOrder] = useState([])
     const [isLoading, setIsLoading] = useState(false)
     const [toggle, setToggle] = useState(false)
 
-
+    const getOrder = () => {
+        setIsLoading(true)
+        shoppingCart.currentOrder('orders')
+        .then((order) => {
+            setOrder(order[0].id)
+            setIsLoading(false)        
+        })
+    }
+    const handleDelete = (id) => {
+        setIsLoading(true)
+        dataManager.delete('orders', id)
+        .then(() => {setIsLoading(false)
+        props.history.push('/products')})
+    }
     useEffect(() => {
         setIsLoading(true)
         shoppingCart.shoppingCart('products/cart')
         .then((cart) => {
             setCart(cart)
             setIsLoading(false)
+        
         })   
-        .catch((err) => console.error('There as an issue with getting all products:', err));
-        
-        
+        .catch((err) => console.error('There as an issue with getting all products:', err));  
     },[toggle]);
+
+    useEffect(() => {
+        getOrder()
+    },[])
 
 
     return(
@@ -27,6 +45,7 @@ const OrderDetails = (props) => {
             <div className="product-container">
             {cart.map((singleProduct, i) => <OrderProductCard setToggle={setToggle} toggle={toggle} key={i+1} product={singleProduct}/>)}
             </div>
+            <button className="delete-order" onClick = {()=>handleDelete(order)} disabled={isLoading}>Delete</button>
         </div>
     )
 }
