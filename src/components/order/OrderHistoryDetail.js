479f679509b76e6df9bfbd819904bba68fb2e868
David Everett <davideverett1989@gmail.com>
2020-09-03 14:33:41 -0500

    wip

--- src/components/order/OrderHistoryDetail.js
+++ src/components/order/OrderHistoryDetail.js
@@ -5,34 +5,51 @@ import dataManager from '../../modules/dataManager';
 
 const OrderHistoryDetail = ({ match }) => {
     const [order, setOrder] = useState({});
-    // const [totalCost, setTotalCost] = useState(0);
+    const [totalCost, setTotalCost] = useState(0);
+
+    const getSingleOrder = () => {
+        const { orderId } = match.params
+        dataManager.get('orders', orderId)
+          .then((order) => {
+              setOrder(order);
+              console.log(order.products);
+          })
+          .catch((err) => console.error('There was an issue getting this order:', err));
+    }
+    const getOrderTotal = () => {
+        let thisOrderTotal = 0;
+        if (order.products) {
+            order.products.forEach((product) => {
+                const productPrice = parseInt(product.price);
+                thisOrderTotal += productPrice;
+            })
+            setTotalCost(thisOrderTotal);
+        }
+    }
 
     useEffect(() => {
-        const getSingleOrder = () => {
-            const { orderId } = match.params
-            dataManager.get('orders', orderId)
-              .then((order) => {
-                  setOrder(order);
-                  console.log(order.products);
-              })
-              .catch((err) => console.error('There was an issue getting this order:', err));
-        }
         getSingleOrder();
     }, []);
 
-    const getOrderTotal = () => {
-        let thisOrderTotal = 0;
-        order.products.forEach((product) => {
-            const productPrice = parseInt(product.price);
-            thisOrderTotal += productPrice;
-            return thisOrderTotal
-        })
-    }
+    useEffect(() => {
+        getOrderTotal();
+    }, [order]);
 
 return (
-    <div className="OrderHistoryCard">
+    <div className="OrderHistoryCard text-center">
         <h1>Order #{order.id}</h1>
-        <h2>Total Cost: ${getOrderTotal()}</h2>
+        <h2>Total Cost: ${totalCost.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')}</h2>
+        <ul className="list-group col-4 m-auto">
+            {
+                order.products ? 
+                order.products.map((x) => (
+                    <li key={x.id} className="list-group-item">
+                        <p>{x.title}</p>
+                        <p>$ {parseInt(x.price).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')}</p>
+                    </li>
+                )) : ''
+            }
+        </ul>
     </div>
 )
 };
